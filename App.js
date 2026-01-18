48b8d82bf42426ca4e1334edd71b6e05ee4c4717
thtun0709 <thanhtung07092004@gmail.com>
2026-01-18 14:54:38 +0700

    Pushing skeleton of mobile

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>chạy được r đó code đi cha</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
